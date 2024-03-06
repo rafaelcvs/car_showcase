9cd145eca1a27a6db2bca27fa67c83d2eea7fb7e
rafaelcvs <rafaelcvs18@hotmail.com>
2024-03-06 16:39:38 +1000

    second commit

---
+++ components/index.ts
@@ -0,0 +1,22 @@
+
+import CarCard from "./CarCard";
+import CustomButton from "./CustomButton";
+import CustomFilter from "./CustomFilter";
+import Footer from "./Footer";
+import NavBar from "./Navbar";
+import ShowMore from "./ShowMore";
+import SearchBar from "./Searchbar";
+import Hero from "./Hero";
+import CarDetails from "./CarDetails";
+
+export {
+  Hero,
+  CarCard,
+  CustomButton,
+  CustomFilter,
+  Footer,
+  NavBar,
+  ShowMore,
+  SearchBar,
+  CarDetails,
+};
